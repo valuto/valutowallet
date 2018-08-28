bdf57f42f0da3c9daea275f28435050f4271caef
Kristian Just Kastrup <kristian@justiversen.dk>
2018-08-28 09:05:25 +0200

    Fixed migration role.

--- migrations/20180706_new_roles_and_user_roles_tables.sql
+++ migrations/20180706_new_roles_and_user_roles_tables.sql
@@ -11,6 +11,6 @@ CREATE TABLE `user_roles` (
   `created_at` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`relation_id`));
 
-INSERT INTO `roles` (`role_id`, `created_at`) VALUES ('vlumarket-admin', '2018-07-06 01:00:00');
+INSERT INTO `roles` (`role_id`, `created_at`) VALUES ('vlumarketadmin', '2018-07-06 01:00:00');
 INSERT INTO `roles` (`role_id`, `created_at`) VALUES ('usercheck', '2018-07-06 01:00:00');
 INSERT INTO `roles` (`role_id`, `created_at`) VALUES ('usercreate', '2018-07-06 01:00:00');
