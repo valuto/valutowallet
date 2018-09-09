3068c1f49ef1716c46f6a805bd0623ca77f35c16
Kristian Just Kastrup <kristian@justiversen.dk>
2018-09-10 00:12:26 +0200

    Added line of trending products from VLU Market.

--- src/Services/Vlumarket/TrendingProducts.php
+++ src/Services/Vlumarket/TrendingProducts.php
@@ -2,19 +2,26 @@
 
 namespace Services\Vlumarket;
 
+use GuzzleHttp\Exception\ClientException;
+
 class TrendingProducts
 {
     public static function get()
     {
         // @TODO add cache
-        
+
         $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', env('VLU_MARKET_URL') . '/api/v1/products/trending', [
-            'connect_timeout' => 4,
-        ]);
+
+        try {
+            $res = $client->request('GET', env('VLU_MARKET_URL') . '/api/v1/products/trending', [
+                'connect_timeout' => 4,
+            ]);
+        } catch (ClientException $e) {
+            return '[]';
+        }
 
         if ($res->getStatusCode() !== 200) {
-            return [];
+            return '[]';
         }
 
         return (string)$res->getBody();
--- view/footer.php
+++ view/footer.php
@@ -27,7 +27,7 @@
 
         </footer>
 
-        <script src="/assets/js/vue.js"></script>
+        <script src="/assets/js/vue.min.js"></script>
         <?php if (!empty($_SESSION['user_session'])): ?>
         <script src="/assets/js/vueapp.js"></script>
         <?php endif; ?>
